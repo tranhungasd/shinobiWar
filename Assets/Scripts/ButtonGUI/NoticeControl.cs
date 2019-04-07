0c0b691eb14c72f652187f3e476242d7a9d2e782
PL <linh.huynh.bku.99@hcmut.edu.vn>
2019-04-07 14:39:43 +0700

    update 2:39 pm 7/4/2019, esc F1-->F5, function options

---
+++ Assets/Scripts/ButtonGUI/NoticeControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoticeControl : MonoBehaviour
+{
+    public GameObject noticeObj;
+    public GameObject btnNotice;
+    private bool status = false;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        GetInput();
+    }
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.F5) && Input.GetKey(KeyCode.F5))
+        {
+            btnNotice.SetActive(status);
+            status = !status;
+            noticeObj.SetActive(status);
+        }
+    }
+    public void quitClick(GameObject gO)
+    {
+        btnNotice.SetActive(status);
+        status = false;
+        noticeObj.SetActive(status);
+    }
+    public void hotkeyClick(GameObject gO)
+    {
+        btnNotice.SetActive(status);
+        status = !status;
+        noticeObj.SetActive(status);
+    }
+}
---
+++ Assets/Scripts/getKeyOptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class getKeyOptions : MonoBehaviour
+{
+    public TextMeshProUGUI txtBtn;
+    private TextMeshProUGUI item;
+    void Start()
+    {
+        item = GetComponent<TextMeshProUGUI>();
+        item.text = formatText(txtBtn.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        item = GetComponent<TextMeshProUGUI>();
+        item.text = formatText(txtBtn.text);
+    }
+    private string formatText(string txt)
+    {
+        if (txt.Length > 3)
+        {
+            return txt[txt.Length - 1].ToString();
+        }
+        return txt;
+    }
+}
