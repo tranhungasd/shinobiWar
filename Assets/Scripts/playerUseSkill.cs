702957ea262f028221ae852501e6821d9de0501f
PL <linh.huynh.bku.99@hcmut.edu.vn>
2019-04-07 20:21:51 +0700

    update 8:21 pm

---
+++ Assets/Scripts/playerUseSkill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerUseSkill : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody2D;
+    private Animator myAnimator;
+    private Vector2 direction;
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
