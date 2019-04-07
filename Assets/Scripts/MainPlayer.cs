282902af54058491d9b34fe3f84a9fb40c068b36
PL <linh.huynh.bku.99@hcmut.edu.vn>
2019-04-07 18:38:44 +0700

    blend tree player

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -31,15 +31,17 @@ public class MainPlayer : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         HandleMovement(horizontal);
     }
-        private void HandleMovement(float horizontal)
+    private void HandleMovement(float horizontal)
     {
         Flip(horizontal);
         myRigidbody2D.velocity = new Vector2(horizontal * speed, myRigidbody2D.velocity.y);
         //Debug.Log(horizontal);
         myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
-        if(jump==true) myAnimator.SetBool("Jump", true);
-        if (jump == false) myAnimator.SetBool("Jump", false);
-        if (slide == false) myAnimator.SetBool("Slide", false);
+        //if(jump==true) myAnimator.SetBool("Jump", true);
+        //if (jump == false) myAnimator.SetBool("Jump", false);
+        //if (slide == false) myAnimator.SetBool("Slide", false);
+        if (jump == true) setJump();
+
     }
     private void Flip(float horizontal)
     {
@@ -54,9 +56,11 @@ public class MainPlayer : MonoBehaviour
     private void GetInput()
     {
         direction = Vector2.zero;
+        setIdle();
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             direction += Vector2.left;
+            setRun();
             if (Input.GetKeyDown(KeyCode.DownArrow) && grounded)
             {
                 StartCoroutine(SlideController());
@@ -66,37 +70,67 @@ public class MainPlayer : MonoBehaviour
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             direction += Vector2.right;
+            setRun();
             if (Input.GetKeyDown(KeyCode.DownArrow) && grounded)
             {
                 StartCoroutine(SlideController());
                 slide = false;
             }
-        }   
+        }
         if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
             jump = true;
             grounded = false;
         }
-        
+
     }
     private IEnumerator SlideController()
     {
         float timePassed = 0;
         while (timePassed < 1)
         {
-            myAnimator.SetBool("Slide", true);
+            //myAnimator.SetBool("Slide", true);
+            setCDN();
             timePassed += Time.deltaTime;
 
             yield return null;
         }
     }
-    private void OnCollisionEnter2D(Collision2D  other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "ground")
         {
             grounded = true;
             jump = false;
         }
+    }
+    private void setCDN()
+    {
+        myAnimator.SetFloat("cdn", 1);
+        myAnimator.SetFloat("idle", 0);
+        myAnimator.SetFloat("run", 0);
+        myAnimator.SetFloat("jump", 0);
+    }
+    private void setRun()
+    {
+        myAnimator.SetFloat("cdn", 0);
+        myAnimator.SetFloat("idle", 0);
+        myAnimator.SetFloat("run", 1);
+        myAnimator.SetFloat("jump", 0);
+    }
+    private void setJump()
+    {
+        myAnimator.SetFloat("cdn", 0);
+        myAnimator.SetFloat("idle", 0);
+        myAnimator.SetFloat("run", 0);
+        myAnimator.SetFloat("jump", 1);
+    }
+    private void setIdle()
+    {
+        myAnimator.SetFloat("cdn", 0);
+        myAnimator.SetFloat("idle", 1);
+        myAnimator.SetFloat("run", 0);
+        myAnimator.SetFloat("jump", 0);
     }
 }
