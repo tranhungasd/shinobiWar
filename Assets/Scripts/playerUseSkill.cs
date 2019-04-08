aa10253fd82ce30c79557870e8e5e39b39b81e63
PL <linh.huynh.bku.99@hcmut.edu.vn>
2019-04-08 18:32:40 +0700

    tra ve cu

--- Assets/Scripts/playerUseSkill.cs
+++ Assets/Scripts/playerUseSkill.cs
@@ -6,41 +6,72 @@ public class playerUseSkill : MonoBehaviour
 {
     private Rigidbody2D myRigidbody2D;
     private Animator myAnimator;
-    private Vector2 direction;
+    private SpriteRenderer sp;
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         GetInput();
     }
     private void GetInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !myAnimator.GetBool("attack"))
         {
-            StartCoroutine(Attack());
-            
+            startAttack();
+            StartCoroutine(AttackNormal());
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && !myAnimator.GetBool("attack"))
+        {
+            startAttack();
+            StartCoroutine(Attack1());
+        }
+        if (Input.GetKeyDown(KeyCode.E) && !myAnimator.GetBool("attack"))
+        {
+            startAttack();
+            StartCoroutine(Attack3());
         }
     }
-    private IEnumerator Attack()
+    private IEnumerator AttackNormal()
     {
-        //animator.SetLayerWeight(animator.GetLayerIndex(layerName), 1);
+
+        sword();
+        Debug.Log(sp.sprite.name.ToString());
+        yield return new WaitForSeconds(0.5f);
+        stopAttack();
+    }
+    private IEnumerator Attack1()
+    {
+        shuriken();
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("attack");
+        stopAttack();
+    }
+    private IEnumerator Attack3()
+    {
+        fire();
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("attack");
+        stopAttack();
+    }
+    private void startAttack()
+    {
         resetAttack();
         ActivateLayer("AttackPlayer");
         myAnimator.SetBool("attack", true);
-        sword();
-        fire();
-        yield return new WaitForSeconds(1f);
-        Debug.Log("attack");
-        resetAttack();
+    }
+    private void stopAttack()
+    {
         myAnimator.SetBool("attack", false);
         ActivateLayer("MovePlayer");
         setIdle();
-    }
+    } 
     public void ActivateLayer(string layerName)
     {
         for (int i = 0; i < myAnimator.layerCount; i++)
@@ -73,11 +104,31 @@ public class playerUseSkill : MonoBehaviour
         myAnimator.SetFloat("rasengan", 0);
         myAnimator.SetFloat("resengan2", 0);
     }
+    private void shuriken()
+    {
+        myAnimator.SetFloat("sword", 0);
+        myAnimator.SetFloat("shuriken", 1);
+        myAnimator.SetFloat("fire", 0);
+        myAnimator.SetFloat("rasengan", 0);
+        myAnimator.SetFloat("resengan2", 0);
+    }
     private void setIdle()
     {
         myAnimator.SetFloat("cdn", 0);
         myAnimator.SetFloat("idle", 1);
         myAnimator.SetFloat("run", 0);
         myAnimator.SetFloat("jump", 0);
+    }
+    public IEnumerator SkillFire()
+    {
+        myAnimator.SetFloat("sword", 0);
+        myAnimator.SetFloat("shuriken", 0);
+        myAnimator.SetFloat("rasengan", 0);
+        myAnimator.SetFloat("resengan2", 0);
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            myAnimator.SetFloat("fire", i / 10);
+        }
     }
 }
