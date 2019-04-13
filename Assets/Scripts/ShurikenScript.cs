a1f01e7a5c4e7bbbbb01cd8f74f7a2e7a05d6d44
tri tran <tri.tran.1713657@hcmut.edu.vn>
2019-04-13 22:24:06 +0700

    tri - shuriken deals damage

---
+++ Assets/Scripts/ShurikenScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikenScript : MonoBehaviour
+{
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = GetComponent<EnemyScipt>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "enemy")
+        {
+            //calls ReceivesDamage() in enemy script
+            col.gameObject.GetComponent<EnemyScipt>().ReceivesDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+}
