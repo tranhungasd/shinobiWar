90f6a8b96ac8df4ebd1799e8a53f119ef1b80313
Hung <tranhung26122612@gmail.com>
2019-04-15 11:12:19 +0700

    Map

---
+++ Assets/ControllCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllCamera : MonoBehaviour
+{
+    public Transform Player;
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Map/Scenes/SceneChange.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange1 : MonoBehaviour
+{
+    public void Map()
+    {
+        SceneManager.LoadScene("Map");
+    }
+    public void MiniMap1()
+    {
+        SceneManager.LoadScene("MiniMap1");
+    }
+    public void MiniMap2()
+    {
+        SceneManager.LoadScene("MiniMap2");
+    }
+    public void MiniMap3()
+    {
+        SceneManager.LoadScene("MiniMap3");
+    }
+    public void Round1()
+    {
+        SceneManager.LoadScene("Round 1");
+    }
+    public void Round2()
+    {
+        SceneManager.LoadScene("Round 2");
+    }
+    public void Round3()
+    {
+        SceneManager.LoadScene("Round 3");
+    }
+    public void Map2Round1()
+    {
+        SceneManager.LoadScene("Map2Round1");
+    }
+    public void Map2Round2()
+    {
+        SceneManager.LoadScene("Map2Round2");
+    }
+    public void Map2Round3()
+    {
+        SceneManager.LoadScene("Map2Round3");
+    }
+}
+
